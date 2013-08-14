01052dfc1daf2f8bcc751cfb9709821c38ab0ac3
Zach Cotter <cotter.z@husky.neu.edu>
2013-08-13 22:13:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rooster::Application.config.secret_token = '481be1026dc2245cdc5b8c03a8b79b77427436dc1db72cad94177ace09ae6798a42a95bba05a62aeb84ebca0287e982d8038edd4ad2715d9d4670ac59a16f904'
